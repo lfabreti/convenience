903b09ffbf7616fe672e0e14c079466e6c2665f0
Luiza Guimaraes Fabreti <lfabret@wla033.palmuc.intranet>
2021-05-27 16:08:38 +0200

    added option to plot different symbols for pairwise comparisons

--- R/plotDiffSplits.R
+++ R/plotDiffSplits.R
@@ -7,11 +7,12 @@
 #' @param minimumESS The threshold for the ESS, default value is 625
 #' @param fill_color The color for the dots on the plot
 #' @param filename A filename to save the table, if NULL the table will be printed
+#' @param per_run If the plot should distinguish the pairwise comparison between runs with different symbols. Default value is FALSE
 #' @param ... (various) Additional arguments passed to plot()
 #'  
 #' @export
 
-plotDiffSplits <- function(output, minimumESS = 625, fill_color = NULL, filename = NULL, ...){
+plotDiffSplits <- function(output, minimumESS = 625, fill_color = NULL, filename = NULL, per_run = FALSE, ...){
   
   col_threshold <- "gray69"
   
@@ -43,11 +44,15 @@ plotDiffSplits <- function(output, minimumESS = 625, fill_color = NULL, filename
   }
   row.names(ess_min_between_runs) <- row.names(output$tree_parameters$ess)
   
-  frequencies <- vector()
-  differences <- vector()
-  freq_low_ess <- vector()
-  diff_low_ess <- vector()
+  list_freq <- list()
+  list_diff <- list()
+  list_freq_low_ess <- list()
+  list_diff_low_ess <- list()
   for (i in 1:length(output$tree_parameters$frequencies)) {
+    frequencies <- vector()
+    differences <- vector()
+    freq_low_ess <- vector()
+    diff_low_ess <- vector()
     for (j in names(output$tree_parameters$frequencies[[i]])) {
       if ( j %in% row.names(ess_min_between_runs)){
         if ( ess_min_between_runs[j, i] < minimumESS ){
@@ -64,14 +69,19 @@ plotDiffSplits <- function(output, minimumESS = 625, fill_color = NULL, filename
         differences <- c( differences, as.vector(unlist(output$tree_parameters$compare_runs[[i]][j])) )
       }
     }
+    list_freq[[i]] <- frequencies
+    list_diff[[i]] <- differences
+    list_freq_low_ess[[i]] <- freq_low_ess
+    list_diff_low_ess[[i]] <- diff_low_ess
+    
   }
-  frequencies <- frequencies[!is.na(frequencies)]
-  differences <- differences[!is.na(differences)]
+  #frequencies <- frequencies[!is.na(frequencies)]
+  #differences <- differences[!is.na(differences)]
   
   x_axis <- exp_diff_runs[1,]
   y_axis <- exp_diff_runs[2,]
   
-  y_lim <- max(differences, y_axis, diff_low_ess, na.rm = T)
+  y_lim <- max(unlist(list_diff), y_axis, unlist(list_diff_low_ess), na.rm = T)
   y_lim <- y_lim + y_lim*0.5
   
   par(mar = c(4.1, 4.9, 2.1, 1.3))
@@ -87,24 +97,57 @@ plotDiffSplits <- function(output, minimumESS = 625, fill_color = NULL, filename
   plot <- polygon(x_axis, y_axis, col = "gray89", border = NA)
   x_extra <- c(0.0, 0.01, 0.99, 1.0, 0.0)
   y_extra <- c(0.0, min(y_axis), min(y_axis), 0.0, 0.0)
+  
   plot <- polygon(x_extra, y_extra, border = NA, col = "gray89")
   plot <- lines(x_axis, y_axis, col = col_threshold, lwd=2)
+  
   title(xlab = "Split frequency", outer = T, line = -1.1)
   title(ylab = "Difference between splits", outer = T, line = -1.1)
   
-  plot <- points(frequencies, differences, pch = 16, col = fill_color)
-  if (length(freq_low_ess) > 0){
-    plot <- points(freq_low_ess, diff_low_ess, pch = 16, col = "tan1")
-    legend("topright",
-           legend = c(paste("ESS",expression("<"),minimumESS),paste("ESS",expression(">"),minimumESS)),
-           pch = c(16,16), 
-           col = c("tan1",fill_color),
-           box.lty = 2,
-           box.col="gray7",
-           cex = 0.7,
-           inset = 0.01)
+  if( length(output$tree_parameters$frequencies) > 1 & per_run == TRUE ){
+    
+    for (i in 1:length(output$tree_parameters$frequencies)) {
+      plot <- points(unlist(list_freq[i]), unlist(list_diff[i]), pch = i, col = fill_color)
+      if (length(list_freq_low_ess[i]) > 0){
+        plot <- points(unlist(list_freq_low_ess[i]), unlist(list_diff_low_ess[i]), pch = i, col = "tan1")
+        legend("topleft",
+               legend = c(paste("ESS",expression("<"),minimumESS),paste("ESS",expression(">"),minimumESS)),
+               pch = c(16,16), 
+               col = c("tan1",fill_color),
+               box.lty = 2,
+               box.col="gray7",
+               cex = 0.7,
+               inset = 0.01)
+      }
+      legend("topright",
+             legend = names(output$tree_parameters$frequencies),
+             pch = c(1:length(output$tree_parameters$frequencies)),
+             box.lty = 2,
+             box.col = "gray7",
+             cex = 0.7,
+             inset = 0.01)
+    }
+    
+  } else{
+    frequencies <- unlist(list_freq)
+    differences <- unlist(list_diff)
+    freq_low_ess <- unlist(list_freq_low_ess)
+    diff_low_ess <- unlist(list_diff_low_ess)
+    plot <- points(frequencies, differences, pch = 16, col = fill_color)
+    if (length(freq_low_ess) > 0){
+      plot <- points(freq_low_ess, diff_low_ess, pch = 16, col = "tan1")
+      legend("topright",
+             legend = c(paste("ESS",expression("<"),minimumESS),paste("ESS",expression(">"),minimumESS)),
+             pch = c(16,16), 
+             col = c("tan1",fill_color),
+             box.lty = 2,
+             box.col="gray7",
+             cex = 0.7,
+             inset = 0.01)
+    }
+    
   }
- 
+  
   if( !(is.null(filename)) ){
     dev.off()
   } 
